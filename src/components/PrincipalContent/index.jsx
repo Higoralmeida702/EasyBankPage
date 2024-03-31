148de06c7411946fbb1151fbb5d02eb765c91348
Higoralmeida702 <higoralmeida7022@gmail.com>
2024-03-31 00:31:34 -0300

    Deixando o site responsivo
    Até o momento o site totalmente responsivo até 1366px.
    
    Pequeno ajuste no margin-left do site, saindo de 200px para 100px.

--- src/components/PrincipalContent/index.jsx
+++ src/components/PrincipalContent/index.jsx
@@ -15,7 +15,7 @@ const PrincipalContent = () => {
             <h1>Why choose Easybank?</h1>
             <p>We leverage Open Banking to turn your bank account into your finalcial hub. <br/>Control your finances like never before.</p>
         <section className="benefits">
-            <div className='bennefitsContent'>
+            <div className='benefitsContent'>
                 <img src="./images/icon-online.svg" alt="" />
                 <h1>Online Banking</h1>
                 <p>Our modern web and mobile application allow you to keep track of your finances wherever you are in the world.</p>
@@ -25,12 +25,12 @@ const PrincipalContent = () => {
                 <h1>Simple Budgeting</h1>
                 <p>see exactly where your money goes each month. Receive notifications when you're close to hitting your limits.</p>
             </div>
-            <div className='bennefitsContent'>
+            <div className='benefitsContent'>
                 <img src="./images/icon-onboarding.svg" alt="" />
                 <h1>Fast Onboarding</h1>
                 <p>We don't do branches. Open your account in minutes online and start taking control of your finances right away.</p>
             </div>
-            <div className='bennefitsContent'>
+            <div className='benefitsContent'>
                 <img src="./images/icon-api.svg" alt="" />
                 <h1>Open API</h1>
                 <p>Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.</p>
